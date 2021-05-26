e5ebf8fef319e798e7e309658df801dd5b8077f1
Nikita <veryrarestar@gmail.com>
2021-05-26 16:28:24 +0300

    Rocket Height

--- Rocket/Assets/Scripts/BGScroll.cs
+++ Rocket/Assets/Scripts/BGScroll.cs
@@ -1,30 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 public class BGScroll : MonoBehaviour
 {
     private Material _material;
     private Vector2 _offset;
-    [Range(-2,2)] public  float _xVelocity = 0;
-    [Range(-2,2)] public  float _yVelocity;
+    [Range(-2, 2)] public float _xVelocity = 0;
+    [Range(-2, 2)] public float _yVelocity;
     [SerializeField] private RocketMovement _rocketMovement;
-    
+
+
 
     private void Awake()
     {
+        enabled = false;
         _rocketMovement = FindObjectOfType<RocketMovement>();
         _material = GetComponent<Renderer>().material;
         _offset = new Vector2(_xVelocity, _yVelocity);
     }
-    
+
     void Update()
     {
         ScrollFromRocketDir();
         _material.mainTextureOffset += _offset * Time.deltaTime;
     }
 
-  
+
     private void ReinitializeOffset()
     {
         _offset = new Vector2(_xVelocity, _yVelocity).normalized * 0.5f;
