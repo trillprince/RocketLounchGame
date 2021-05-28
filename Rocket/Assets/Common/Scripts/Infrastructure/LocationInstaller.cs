ac923051bbf8c5d61e234deef020e912aa718785
Nikita <veryrarestar@gmail.com>
2021-05-28 15:49:09 +0300

    Zenject begining

---
+++ Rocket/Assets/Common/Scripts/Infrastructure/LocationInstaller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Common.Scripts.Infrastructure
+{
+    public class LocationInstaller: MonoInstaller
+    {
+        [SerializeField] private Transform _rocketSpawnPoint;
+        [SerializeField] private GameObject _rocketPrefab;
+        public override void InstallBindings()
+        {
+            BindRocket();
+        }
+
+        private void BindRocket()
+        {
+            RocketMovement rocketMovement =
+                Container.InstantiatePrefabForComponent<RocketMovement>(_rocketPrefab, _rocketSpawnPoint.position,
+                    Quaternion.identity, null);
+
+            Container
+                .Bind<RocketMovement>()
+                .FromInstance(rocketMovement)
+                .AsSingle()
+                .NonLazy();
+        }
+    }
+}
